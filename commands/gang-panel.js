da5496c730b99cb9b62d7677c0e1dbcf884fba24
Lupin <sncmarcpollapira00204@gmail.com>
2026-04-03 23:23:43 +0800

    Add files via upload

--- commands/gang-panel.js
+++ commands/gang-panel.js
@@ -46,11 +46,11 @@ module.exports = {
             interaction.guild.iconURL({ dynamic: true })
         )
         .setImage(
-            "https://cdn.discordapp.com/attachments/1466355570805313719/1466355571706826842/Poblagif1.gif?ex=69d0d18b&is=69cf800b&hm=6241a4243e099977b144c4aedf2535dac1d9fe01ed439852c12c3f9d5b53af66"
+            "https://cdn.discordapp.com/attachments/1466355570805313719/1466783941867339888/gif_tag_3.gif?ex=69d10eff&is=69cfbd7f&hm=cb7260fac014cc0e2128d919d99ea46e6c58246fb6a7f435a58d92ae6623a7bc"
         )
         .setFooter({
             text: "Poblacion City Roleplay",
-            iconURL: "https://cdn.discordapp.com/attachments/1466355570805313719/1466783941867339888/gif_tag_3.gif?ex=69d10eff&is=69cfbd7f&hm=cb7260fac014cc0e2128d919d99ea46e6c58246fb6a7f435a58d92ae6623a7bc"
+            iconURL: "https://cdn.discordapp.com/attachments/1466355570805313719/1466355571706826842/Poblagif1.gif?ex=69d0d18b&is=69cf800b&hm=6241a4243e099977b144c4aedf2535dac1d9fe01ed439852c12c3f9d5b53af66"
         });
 
 
