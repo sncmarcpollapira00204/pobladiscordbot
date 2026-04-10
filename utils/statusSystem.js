dce31d7467518805d900e96c9a32940ec20a9b9e
Lupin <sncmarcpollapira00204@gmail.com>
2026-04-10 15:25:19 +0800

    Add files via upload

--- utils/statusSystem.js
+++ utils/statusSystem.js
@@ -11,7 +11,7 @@ const {
 
 module.exports = (client) => {
 
-  const STATUS_CHANNEL_ID = "1465003991208558724";
+  const STATUS_CHANNEL_ID = "1485360344238002186";
 
   let statusMessage = null;
   let lastPayload = "";
